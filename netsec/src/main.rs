6b06b63c2a30b528b9d5f0991f7cf3ad0e564e28
Evan Schoening <evan.schoening@gmail.com>
2024-02-21 16:57:59 -0800

    added for ip filtering, will get to port scanning

--- netsec/src/main.rs
+++ netsec/src/main.rs
@@ -11,11 +11,16 @@ fn main() {
     // 1. Discover devices on network with ping
     let base_ip = Ipv4Addr::new(192, 168, 1, 1);
     // TODO this can be multi-threaded 
+    let mut valid_ips = vec![];
     for num in 1..255 {
         let test_ip = Ipv4Addr::new(base_ip.octets()[0], base_ip.octets()[1], base_ip.octets()[2], num);
         println!("{:?}", test_ip);
-        ping_tool::ping_address(&test_ip.to_string())
+        let ip  = match ping_tool::ping_address(&test_ip.to_string()) {
+            Some(ip) => valid_ips.push(ip),
+            _ => println!("IP not valid")
+        };
     }
+    println!("IPs {:?},", {valid_ips})
     //ping_tool::ping_address("192.168.1.1")
 }
 
--- netsec/src/ping_tool.rs
+++ netsec/src/ping_tool.rs
@@ -1,15 +1,19 @@
 // use this one https://docs.rs/pinger/latest/pinger/all.html#functions
 use pinger::{ping, PingResult};
+use std::net::{TcpStream, SocketAddr};
+use std::io::{Error, ErrorKind};
 
-pub fn ping_address(address: &str) {
+pub fn ping_address(address: &str) -> Option<String> {
     let stream = ping(address.to_string(), None).expect("Error pinging");
     let mut count = 0;
+    let mut valid_addr: Option<String> = None;
     for message in stream {
         if count >5 {
-            return 
+            return valid_addr
         }
         match message {
-            PingResult::Pong(duration, _) => println!("{:?} : {:?}", duration, address),
+            PingResult::Pong(duration, _) => {println!("{:?} : {:?}", duration, address);
+                                                valid_addr = Some(address.to_string())},
             PingResult::Timeout(_) => println!("Timeout!"),
             // Unknown lines, just ignore.
             PingResult::Unknown(line) => (),
@@ -17,13 +21,35 @@ pub fn ping_address(address: &str) {
         }
         count +=1;
     }
+    return valid_addr
 }
 
 
-pub fn port_scanner(ports: &vec[u16]){
+pub fn port_scanner(address: &str, ports: Option<&Vec<u16>>){
+    // Need to check if ports
+    let standard_ports = vec![21,22,25,53,80,135,139,443,445,1723,3389];
+
+    let search_ports = match ports {
+        Some(ports) => ports,
+        _ => &standard_ports
+    };
+    for port in search_ports { 
+        let addr = format!("{}:{}", address, port);
+        // convert addr into socket addr
+        let socket_addr = match addr.parse::<SocketAddr>() {
+            Ok(addr) => addr,
+            Err(e) => panic!("Failed to parse addr: {}", e)
+        };
+        match TcpStream::connect_timeout(&socket_addr, std::time::Duration::from_secs(1)) {
+            Ok(_) => println!("Port {} is open", port),
+            Err(e) => match e.kind() {
+                ErrorKind::TimedOut => println!("Port {} is closed", port),
+                _ => println!("Failed to connect: {}", e),
+            },
+        }
+    }
     return
-    use std::net::{TcpStream, SocketAddr};
-use std::io::{Error, ErrorKind};
+
 
 // fn main() {
 //     let target = "127.0.0.1";
